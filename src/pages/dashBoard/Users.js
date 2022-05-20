8ba4adf477a6c1379360223baf4c82efdcc5c209
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-05-21 00:28:42 +0600

    add admin and delete btn on admin table and undate user role: admin fild

--- src/pages/dashBoard/Users.js
+++ src/pages/dashBoard/Users.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useQuery } from 'react-query';
+import auth from '../../firebaseConfig';
 
 const Users = () => {
+    const [user, loading] = useAuthState(auth)
 
     const { isLoading, error, data: users } = useQuery('users', () =>
     fetch('http://localhost:5000/users').then(res =>
       res.json()
-    ))
-
+        ))
+    
+    // handle admon
+    const handleAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${user?.email}`, {
+            method: 'PUT',
+            headers: {
+                'authorozation': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+        })
+    }
     return (
         <div className='w-full'>
             <h1>Doctors Portal Users : {Users?.length}</h1>
@@ -24,6 +40,22 @@ const Users = () => {
                             <tr key={user._id}>
                                 <th>{index}</th>
                                 <td>{user.email}</td>
+                                <td><button onClick={handleAdmin} className="btn btn-ghost">Admin</button></td>
+                                <td>
+                                    <button
+                                        className="btn btn-square btn-outline">
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            className="h-5 w-5" fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor">
+                                            <path strokeLinecap="round"
+                                                troke-linejoin="round"
+                                                strokeWidth="2"
+                                                d="M6 18L18 6M6 6l12 12"
+                                            />
+                                        </svg>
+                                    </button>
+                                </td>
                             </tr>
                         )}
                         
